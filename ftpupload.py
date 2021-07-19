bfe77cc51ecf6ea0322ab676ee5ef72bc55fe912
Christian Nielsen <nielsen@amrose.dk>
2021-07-19 20:15:31 +1000

    .

--- ftpupload.py
+++ ftpupload.py
@@ -9,14 +9,15 @@ code = None
 while(True):
     files = glob.glob("./video/*full.avi")
     if len(files) > 0:
-        session = ftplib.FTP('192.168.1.1','admin','admin')
+        session = ftplib.FTP('192.168.1.1', 'admin', 'admin')
         session.cwd('volume(sda1)')
         session.cwd('trapcam')
         for filename in files:
-            file = open(filename,'rb')                  # file to send
-            code = session.storbinary(f'STOR {ntpath.basename(filename)}', file)     # send the file
+            file = open(filename, 'rb')                  # file to send
+            code = session.storbinary('STOR {}'.format(
+                ntpath.basename(filename)), file)     # send the file
             print(code)
-            file.close()                      
+            file.close()
             if code.find('226') > -1:
                 os.remove(filename)              # close file and FTP
         session.quit()
