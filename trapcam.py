232a590d30a0cc54a956a8685fc13707a426eb19
Christian Nielsen <nielsen@amrose.dk>
2021-07-20 22:35:18 +1000

    Update trapcam.py

--- trapcam.py
+++ trapcam.py
@@ -1,91 +1,94 @@
+import glob
 import cv2
 import numpy as np
 from datetime import datetime
 import time
 import os
 
-cap = cv2.VideoCapture(0)
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
+def rename_part_files():
+    files = glob.glob("/tmp/trapcam/*part.avi")
+    for filename in files:
+        os.rename(filename, filename.replace("part.avi", ".avi"))
 
-out = None
+def make_blur(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    return cv2.GaussianBlur(gray, (21, 21), 0)
 
-reference_frame = None
-reference_blur = None
-i = 0  # number of frames since movement started
-d = 0
-#
-f = 1
-min_moment = 50000
-started = False
-buffered_frames = []
-path = ""
+def imprint_datetime(now, frame):
+    cv2.putText(frame, "{}".format(now), (0,470), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
 
-while(1):
-    ok, frame = cap.read()
-    if ok:
+def calculate_moment(reference_blur, blur):
+    diff = cv2.absdiff(reference_blur, blur)
+    thresh = cv2.threshold(diff, 20, 255, cv2.THRESH_BINARY)[1]
+    dilate = cv2.dilate(thresh, None, 2)
+    return cv2.moments(dilate)['m00']
 
-        now = datetime.now()
+def start_recording(now, buffered_frames):
+    filename = '/tmp/trapcam/{0}part.avi'.format(now.strftime("%Y-%m-%d_%H-%M-%S"))
+    print('start recording video file:', filename)
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(filename, fourcc, 20.0, (640,  480))
+    for f in buffered_frames:
+        out.write(f)
+    return out
 
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        blur = cv2.GaussianBlur(gray, (21, 21), 0)
-
-        if reference_frame is None:
-            reference_frame = frame
-            reference_blur = blur
-
-        diff = cv2.absdiff(reference_blur, blur)
-        thresh = cv2.threshold(diff, 20, 255, cv2.THRESH_BINARY)[1]
-        dilate = cv2.dilate(thresh, None, 2)
-        moment = cv2.moments(dilate)['m00']
-
-        print("{} {}".format(d, moment))
-
-        cv2.putText(frame, "{}".format(now), (0, 470), cv2.FONT_HERSHEY_SIMPLEX,
-                    0.5, (255, 255, 255), 1, cv2.LINE_AA)
-
-        if moment > min_moment:
-            if d >= 50 and not started:
-                path = './video/{}'.format(now.strftime("%Y-%m-%d_%H-%M-%S"))
-                print('start writing video file:', path)
-                out = cv2.VideoWriter(path+'.avi', fourcc, 10.0, (640,  480))
-                for f in buffered_frames:
-                    out.write(f)
-                started = True
-                i = 0
-            elif not started:
-                buffered_frames.append(frame)
-            d = min(d + 10, 100)
-
-        else:
-            if d <= -50 and started:
-                print('stop')
-                out.release()
-                os.rename(path+'.avi', path+'full.avi')
-                started = False
-            d = max(d - 3, -100)
-            buffered_frames = []
-
-            #cv2.imwrite(f'./frames/frame-{f:05d}-ref.jpg', reference_frame)
-            #cv2.imwrite(f'./frames/frame-{f:05d}-{moment}.jpg', frame)
-
-        if started:
-            out.write(frame)
-            #cv2.imshow('capture', frame)
-
-        reference_frame = frame
-        reference_blur = blur
-
-        #k = cv2.waitKey(1) & 0xFF
-        # if k == 13:
-        #    break
-    else:
-        print("Failded to capture frame")
-        break
-
-    time.sleep(0.1)
-
-if started:
+def stop_recording(out):
+    print('stop recording')
     out.release()
-    os.rename(path+'.avi', path+'full.avi')
-cap.release()
-# cv2.destroyAllWindows()
+
+def capture(cap):
+    d = 0
+    started = False;
+    reference_blur = None
+    buffered_frames = []
+    out = None
+    try:
+        while(1):
+            ok, frame = cap.read()
+            if not ok:
+                print ("Failded to capture frame")
+                break
+            else:
+                blur = make_blur(frame)
+                now = datetime.now()
+                imprint_datetime(now, frame)
+                cv2.imshow('capture', frame)
+                
+                if not reference_blur is None:
+                    moment = calculate_moment(reference_blur, blur)
+                    print ("{0} {1}".format(d, moment))
+            
+                    if moment > 50000:
+                        if d >= 50 and not started:
+                            out = start_recording(now, buffered_frames)
+                            started = True
+                        elif not started:
+                            buffered_frames.append(frame)
+                        d = min(d + 10, 100)
+                        
+                    else:
+                        if d <= -50 and started:
+                            stop_recording(out)
+                            out = None
+                            started = False
+                            rename_part_files()
+                        d = max(d - 3, -100)
+                        buffered_frames = []
+               
+                if started:
+                    out.write(frame)
+                reference_blur = blur
+    finally:
+        if started:
+            out.release()
+
+while True:
+    rename_part_files()
+    cap = cv2.VideoCapture(0)
+    try:
+        capture(cap)
+    except Exception as ex:
+        print("Error:", ex)
+    finally:
+        cap.release()
+        cv2.destroyAllWindows()
