701c56f76ceaa5954f6bf25d5b88096ea16f7c8f
Christian Nielsen <nielsen@amrose.dk>
2021-07-31 15:20:04 +1000

    Added config file

--- ftpupload.py
+++ ftpupload.py
@@ -3,16 +3,21 @@ import ftplib
 import os
 import ntpath
 import time
+import configparser
 
 
 def transfer_file(file):
-    session = ftplib.FTP('192.168.1.1', 'admin', 'admin')
+    config = configparser.ConfigParser()
+    config.read('trapcam.ini')
+    hostname = config['ftpupload']['hostname']
+    user = config['ftpupload']['user']
+    password = config['ftpupload']['password']
+    folder = config['ftpupload']['folder']
+    session = ftplib.FTP(hostname, user, password)
     try:
-        session.cwd('volume(sda1)')
-        session.cwd('trapcam')
+        for f in folder.split('/'):
+            session.cwd(f)
         return session.storbinary('STOR {0}'.format(ntpath.basename(file.name)), file).startswith('226')
-    except:
-        return False
     finally:
         session.quit()
 
@@ -21,8 +26,6 @@ def open_and_transfer_file(filename):
     file = open(filename, 'rb')
     try:
         return transfer_file(file)
-    except:
-        return False
     finally:
         file.close()
 
