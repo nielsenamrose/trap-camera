eabbfe8d822278bbe1ce252b2921b7ad94b875ec
Christian Nielsen <nielsen@amrose.dk>
2021-07-31 13:05:27 +1000

    Turn off blue leds after two minutes

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
 #! /bin/sh
 python3 ftpupload.py &
 python3 trapcam.py &
+
+sleep 2m
+echo 0 > /sys/class/leds/beaglebone:green:usr0/brightness
+echo 0 > /sys/class/leds/beaglebone:green:usr1/brightness
+echo 0 > /sys/class/leds/beaglebone:green:usr2/brightness
+echo 0 > /sys/class/leds/beaglebone:green:usr3/brightness
+echo 0 > /sys/class/leds/wl18xx_bt/brightness
+exit 0
