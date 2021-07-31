a50903acd94bcbb6825b6d4963b5b6f19ebaf85f
Christian Nielsen <nielsen@amrose.dk>
2021-07-31 13:33:23 +1000

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,5 +7,5 @@ echo 0 > /sys/class/leds/beaglebone:green:usr0/brightness
 echo 0 > /sys/class/leds/beaglebone:green:usr1/brightness
 echo 0 > /sys/class/leds/beaglebone:green:usr2/brightness
 echo 0 > /sys/class/leds/beaglebone:green:usr3/brightness
-echo 0 > /sys/class/leds/wl18xx_bt/brightness
+echo 0 > /sys/class/leds/wl18xx_bt_en/brightness
 exit 0
