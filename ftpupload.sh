e313c2f9af41ea4f6da358c6edd8caee0cc316a9
Christian Nielsen <nielsen@amrose.dk>
2023-02-23 13:17:22 +1000

    simpler ftpupload.sh

--- ftpupload.sh
+++ ftpupload.sh
@@ -4,10 +4,9 @@ username=user
 password=user
 
 while true; do
-	for filename in *[!part]?.avi; do
+	for filename in *[!part]?.avi*; do
 		if [ -f $filename ]; then
 			curl -u $username:$password -T $filename $url && rm -f $filename
-			curl -u $username:$password -T $filename.jpg $url && rm -f $filename.jpg
 		fi
 	done
 	sleep 1m
